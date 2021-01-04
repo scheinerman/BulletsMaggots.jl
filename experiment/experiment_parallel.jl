cf39fac9f07bfbcf26d7bfae041f109943380d14
Ed Scheinerman <ers@jhu.edu>
2021-01-04 10:29:56 -0500

    Clean up formatting

--- experiment/experiment_parallel.jl
+++ experiment/experiment_parallel.jl
@@ -5,17 +5,17 @@ println("nprocs = ", nprocs())
 @everywhere using BulletsMaggots
 @everywhere build_table(false)
 
-function jonah_parallel(reps::Int=1000)
-    total = @distributed (+) for i=1:reps
-        jonah_solver(guess_any(),false)
-end
-return total/reps
+function jonah_parallel(reps::Int = 1000)
+    total = @distributed (+) for i = 1:reps
+        jonah_solver(guess_any(), false)
+    end
+    return total / reps
 end
 
 
-function bm_parallel(reps::Int=1000)
-    total = @distributed (+) for i=1:reps
-        bm_solver(guess_any(),false)
-end
-return total/reps
+function bm_parallel(reps::Int = 1000)
+    total = @distributed (+) for i = 1:reps
+        bm_solver(guess_any(), false)
+    end
+    return total / reps
 end
