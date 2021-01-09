7ae4d3821c706c7d50eb7785184851aae067fd44
Ed Scheinerman <ers@jhu.edu>
2021-01-09 15:45:01 -0500

    Better shell script

--- experiment/main_parallel.jl
+++ experiment/main_parallel.jl
@@ -1,11 +1,14 @@
 include("experiment_parallel.jl")
 
-function run(n::Int = 20)
-    for k = 0:4
+reps = parse(Int, ARGS[1])
+limit = parse(Int, ARGS[2])
+
+function run(n,lim)
+    for k = 0:lim
         println()
         println("Running: jonah_parallel($n,$k)")
         println("Average turns per game = ", jonah_parallel(n, k))
     end
 end
 
-@time run(10_000)
+@time run(reps,limit)
--- experiment/main_parallel.sh
+++ experiment/main_parallel.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo Starting julia with $1 processors
-nohup julia -p $1 main_parallel.jl > ~/experiment_out.txt &
+#
+# ./main_parallel.sh nprocs nreps limit
+echo Starting julia with $1 processors to run $2 repetitions with delays up to $3
+nohup julia -p $1 main_parallel.jl $2 $3 > ~/experiment_out.txt &
