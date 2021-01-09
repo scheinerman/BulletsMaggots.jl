6e2e47f13f08035a705df7fe759427e55c5430e5
Ed Scheinerman <ers@jhu.edu>
2021-01-09 15:58:10 -0500

    Better shell interface

--- experiment/main_parallel.sh
+++ experiment/main_parallel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 # ./main_parallel.sh nprocs nreps limit
-echo Starting julia with $1 processors to run $2 repetitions with delays up to $3
-nohup julia -p $1 main_parallel.jl $2 $3 > ~/experiment_out.txt &
+echo Running with $1 processors with $2 repetitions with delays up to $3 >  ~/experiment_out.txt
+nohup julia -p $1 main_parallel.jl $2 $3 >> ~/experiment_out.txt &
